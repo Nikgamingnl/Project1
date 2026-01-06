14c8b5ce76c1ee18d11fbbf4f15da123a9737639
Nikgamingnl <nikwarmerdam@gmail.com>
2026-01-06 16:52:12 +0100

    hopelijk cleanere code

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,28 @@
 #include <Arduino.h>
 
-const int PIN_DOOD    = 35;
+// Pin definitions
+const int PIN_DEATH   = 35;
 const int PIN_RELOAD  = 34;
 const int PIN_TRIGGER = 17;
 const int PIN_RESET   = 5;
 const int PIN_HIT     = 4;
 const int PIN_SETUP   = 16;
-      int levens      = 0;
 const int PIN_RDR     = 19;
 const int PIN_PVP     = 18;
+
+// Game variables
+int lives = 0;
+int ammo = 0;
+bool magazineInserted = false;
+
+// Timing variables
 unsigned long startMillis;
 unsigned long currentMillis;
-const unsigned long period = 250;
-const unsigned long period2 = 1800;
-const unsigned long period3 = 600;
-const unsigned long period4 = 500;
+const unsigned long HIT_DURATION = 500;
+const unsigned long RELOAD_DURATION_PARTIAL = 1800;  // For partial reload
+const unsigned long RELOAD_DURATION_EMPTY = 600;     // For empty reload
 
+// State machine
 enum State {
   SETUP,
   ALIVE,
@@ -27,11 +34,10 @@ enum State {
 
 State currentState = SETUP;
 
-int ammo = 0;
-
+// Button debouncing variables
 bool prevTrigger = HIGH; 
 bool prevReload  = LOW;  
-bool prevDood    = HIGH;
+bool prevDeath   = HIGH;
 bool prevReset   = HIGH;
 bool prevHit     = HIGH;
 bool prevPVP     = HIGH;
@@ -45,11 +51,12 @@ const unsigned long TRIGGER_DEBOUNCE_MS = 50;
 void setup() {
   Serial.begin(115200);
 
-  pinMode(PIN_DOOD, INPUT_PULLUP);
+  // Configure pins
+  pinMode(PIN_DEATH, INPUT_PULLUP);
   pinMode(PIN_RELOAD, INPUT);
   pinMode(PIN_TRIGGER, INPUT_PULLUP);
   pinMode(PIN_RESET, INPUT_PULLUP);
-  pinMode(PIN_HIT, INPUT_PULLUP);
+  pinMode(PIN_HIT, INPUT);  // IR sensor
   pinMode(PIN_SETUP, INPUT_PULLUP);
   pinMode(PIN_RDR, INPUT_PULLUP);
   pinMode(PIN_PVP, INPUT_PULLUP);
@@ -61,25 +68,28 @@ void loop() {
   // Update current time
   currentMillis = millis();
   
-  // Lees inputs
-  bool doodKnop   = digitalRead(PIN_DOOD);
-  bool reloadKnop = digitalRead(PIN_RELOAD);
-  bool trigger    = digitalRead(PIN_TRIGGER);
-  bool resetKnop  = digitalRead(PIN_RESET);
-  bool hitknop    = digitalRead(PIN_HIT);
-  bool setupknop  = digitalRead(PIN_SETUP);
-  bool rdr        = digitalRead(PIN_RDR);
-  bool pvp        = digitalRead(PIN_PVP);
+  // Read all inputs
+  bool deathButton = digitalRead(PIN_DEATH);
+  bool reloadButton = digitalRead(PIN_RELOAD);
+  bool trigger = digitalRead(PIN_TRIGGER);
+  bool resetButton = digitalRead(PIN_RESET);
+  bool hitSensor = digitalRead(PIN_HIT);
+  bool setupButton = digitalRead(PIN_SETUP);
+  bool rdrButton = digitalRead(PIN_RDR);
+  bool pvpButton = digitalRead(PIN_PVP);
   
-
-  bool doodPressed   = (doodKnop == LOW && prevDood   == HIGH);            // falling edge
-  bool reloadPressed = (reloadKnop == HIGH && prevReload == LOW);         // rising edge (active-high)
-  bool resetPressed  = (resetKnop == LOW && prevReset  == HIGH);         // falling edge
-  bool hitPressed    = (hitknop == LOW && prevHit    == HIGH);           // falling edge
-  bool pvpPressed    = (pvp == LOW && prevPVP    == HIGH);               // falling edge
-  bool rdrPressed    = (rdr == LOW && prevRDR    == HIGH);               // falling edge
-
-
+  // Update magazine status
+  magazineInserted = reloadButton;
+  
+  // Detect button presses (edges)
+  bool deathPressed = (deathButton == LOW && prevDeath == HIGH);
+  bool reloadPressed = (reloadButton == HIGH && prevReload == LOW);
+  bool resetPressed = (resetButton == LOW && prevReset == HIGH);
+  bool hitPressed = (hitSensor == LOW && prevHit == HIGH);
+  bool pvpPressed = (pvpButton == LOW && prevPVP == HIGH);
+  bool rdrPressed = (rdrButton == LOW && prevRDR == HIGH);
+  
+  // Trigger debouncing
   bool triggerPressed = false;
   if (trigger != triggerRawLast) {
     triggerRawLast = trigger;
@@ -87,101 +97,76 @@ void loop() {
   }
   if ((currentMillis - triggerLastChange) > TRIGGER_DEBOUNCE_MS) {
     if (triggerStable != trigger) {
-     
       triggerStable = trigger;
       if (triggerStable == LOW) {
-       
         triggerPressed = true;
       }
     }
   }
-
- 
-  if (triggerPressed) {
- 
-  }
-
-  // Update vorige waarden
-  prevDood   = doodKnop;
-  prevReload = reloadKnop;
-  prevTrigger= trigger;
-  prevReset  = resetKnop;
-  prevHit    = hitknop;
-  prevPVP    = pvp;
-  prevRDR    = rdr;
+  
+  // Update previous states
+  prevDeath = deathButton;
+  prevReload = reloadButton;
+  prevTrigger = trigger;
+  prevReset = resetButton;
+  prevHit = hitSensor;
+  prevPVP = pvpButton;
+  prevRDR = rdrButton;
+  // State machine
   switch (currentState) {
     case SETUP:
-      if (pvpPressed){
-        levens = 3;
+      if (pvpPressed) {
+        lives = 3;
         currentState = ALIVE;
-        
-      } else if (rdrPressed){
-        levens = 1;
+      } else if (rdrPressed) {
+        lives = 1;
         currentState = ALIVE;
-        
       }
       break;
     
     case ALIVE:
-      if (hitPressed){
-        levens = levens - 1;
-        
+      if (hitPressed) {
+        lives--;
         startMillis = currentMillis;
         currentState = HIT;
-      } else if (levens <= 0 || doodPressed) {
+      } else if (lives <= 0 || deathPressed) {
         currentState = DEAD;
-        
-
-      } else if (reloadPressed) {
-        // Start reloading only when ammo isn't full
-        if (ammo < 6) {
-          startMillis = currentMillis;
-          currentState = RELOADING;
-         
-        }
-      } else if (triggerPressed && ammo > 0) {
+      } else if (reloadPressed && ammo < 6) {
+        startMillis = currentMillis;
+        currentState = RELOADING;
+      } else if (triggerPressed && ammo > 0 && magazineInserted) {
         startMillis = currentMillis;
         currentState = SHOOTING;
       }
       break;
 
-      case HIT:
-        if (currentMillis - startMillis >= period4){
-         startMillis = currentMillis;
+    case HIT:
+      if (currentMillis - startMillis >= HIT_DURATION) {
         currentState = ALIVE;
-        }else{
-        currentState = HIT;
-        }
-        break;
+      }
+      // Else remain in HIT
+      break;
       
     case DEAD:
       if (resetPressed) {
         ammo = 0;
-        levens = 0;
-        
+        lives = 0;
         currentState = SETUP;
       }
       break;
 
     case RELOADING:
-      if ((ammo != 6 && ammo != 0 && currentMillis - startMillis >= period2) ||
-          (ammo == 0 && currentMillis - startMillis >= period3)) {
+      if ((ammo != 6 && ammo != 0 && currentMillis - startMillis >= RELOAD_DURATION_PARTIAL) ||
+          (ammo == 0 && currentMillis - startMillis >= RELOAD_DURATION_EMPTY)) {
         ammo = 6;
-     
         currentState = ALIVE;
       }
       break;
     
-
     case SHOOTING:
       ammo--;
-    
-      if (ammo == 0) {
-  
-
-        
-      }
       currentState = ALIVE;
       break;
   }
+}
 }
